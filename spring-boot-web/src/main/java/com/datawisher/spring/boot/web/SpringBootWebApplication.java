5aefd06351cc380ef653eefb4a499515997175bd
Jim Han <Jim.Han2@Honeywell.com>
2020-09-19 00:36:17 +0800

    hikari verify

--- spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
@@ -1,5 +1,9 @@
 package com.datawisher.spring.boot.web;
 
+import java.sql.Connection;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Jim Han
  */
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource datasource;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Connection connection = datasource.getConnection();
+        connection.close();
     }
 }
