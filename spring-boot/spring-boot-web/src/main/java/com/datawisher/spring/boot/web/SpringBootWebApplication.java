15c2546c300d75373636d2211f34a1537fbd73c9
Jim Han <Jim.Han2@Honeywell.com>
2020-12-03 22:38:12 +0800

    dockerfile-maven example

--- spring-boot/spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
+++ spring-boot/spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
 /**
  * @author Jim Han
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class SpringBootWebApplication implements CommandLineRunner {
 
     @Autowired
