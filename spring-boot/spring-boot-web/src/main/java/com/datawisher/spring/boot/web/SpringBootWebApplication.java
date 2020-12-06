d9e6b3a66396cea2c05c27c37b778088ce49988a
Jim Han <Jim.Han2@Honeywell.com>
2020-12-06 09:11:13 +0800

    no repeat by redis

--- spring-boot/spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
+++ spring-boot/spring-boot-web/src/main/java/com/datawisher/spring/boot/web/SpringBootWebApplication.java
@@ -1,5 +1,6 @@
 package com.datawisher.spring.boot.web;
 
+import com.datawisher.spring.boot.web.function.NoRepeat;
 import java.sql.Connection;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * @author Jim Han
@@ -17,6 +22,9 @@ public class SpringBootWebApplication implements CommandLineRunner {
     @Autowired
     private DataSource datasource;
 
+    @Autowired
+    private NoRepeat noRepeat;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
@@ -25,5 +33,7 @@ public class SpringBootWebApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Connection connection = datasource.getConnection();
         connection.close();
+
+        noRepeat.noRepeatByRedis();
     }
 }
