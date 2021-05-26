9f0c3509f7061773a9dfc2d694996f9b6510a204
Jim Han <Jim.Han2@Honeywell.com>
2021-05-26 09:48:40 +0800

    update code

--- spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/controller/PersonController.java
+++ spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/controller/PersonController.java
@@ -4,16 +4,25 @@ import com.datawisher.spring.boot.jdbc.model.Person;
 import com.datawisher.spring.boot.jdbc.service.PersonService;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("person")
 public class PersonController {
 
     @Autowired
     PersonService personService;
 
-    @PostMapping("/person")
+    @GetMapping("/{id}")
+    public Person findPerson(@PathVariable("id") Long id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping("/add")
     public void personOps() {
         Person p1 = new Person();
         p1.setId((long) UUID.randomUUID().hashCode());
