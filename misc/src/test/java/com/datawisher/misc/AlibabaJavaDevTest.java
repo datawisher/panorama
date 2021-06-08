a650f2ed69c3f81fe26b8798a4a3a3624f1db6f0
Jim Han <Jim.Han2@Honeywell.com>
2021-06-08 18:46:05 +0800

    update

--- misc/src/test/java/com/datawisher/misc/AlibabaJavaDevTest.java
+++ misc/src/test/java/com/datawisher/misc/AlibabaJavaDevTest.java
@@ -2,6 +2,10 @@ package com.datawisher.misc;
 
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -48,5 +52,38 @@ public class AlibabaJavaDevTest {
         // BigDecimal 推荐创建用法
         BigDecimal recommend1 = new BigDecimal("0.1");
         BigDecimal recommend2 = BigDecimal.valueOf(0.1);
+    }
+
+    /**
+     * 数组转List注意事项
+     */
+    @Test
+    public void arrayToList() {
+        String[] str = new String[] { "jim", "han" };
+        List list = Arrays.asList(str);
+        Assertions.assertEquals(Arrays.toString(str), list.toString());
+        // 修改了数组中的元素，对应的list也会被修改，Arrays.asList体现的是适配器模式，只是转换接口，后台的数据仍是数组
+        str[0] = "jing";
+        Assertions.assertEquals(Arrays.toString(str), list.toString());
+        // 不能对Arrays.asList使用add，remove，clear等方法，否则会抛出如下异常
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> list.add("qiang"));
+    }
+
+    /**
+     *  循环遍历集合的过程中 add/remove 元素
+     */
+    @Test
+    public void addOrRemoveElementWhenForeach() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        Assertions.assertThrows(ConcurrentModificationException.class, () -> {
+            for (String item : list) {
+                // 如果删除list头元素，并不会抛出异常
+                if ("2".equals(item)) {
+                    list.remove(item);
+                }
+            }
+        });
     }
 }
