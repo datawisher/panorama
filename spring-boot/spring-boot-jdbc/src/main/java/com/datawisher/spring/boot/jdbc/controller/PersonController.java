2dcbb080ef5d3e3e9208a48cb1fcd02c8af81f35
Jim Han <Jim.Han2@Honeywell.com>
2021-11-27 20:15:04 +0800

    [refactor] jdbcTemplate

--- spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/controller/PersonController.java
+++ spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/controller/PersonController.java
@@ -2,11 +2,11 @@ package com.datawisher.spring.boot.jdbc.controller;
 
 import com.datawisher.spring.boot.jdbc.model.Person;
 import com.datawisher.spring.boot.jdbc.service.PersonService;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +23,12 @@ public class PersonController {
     }
 
     @PostMapping("/add")
-    public void personOps() {
-        Person p1 = new Person();
-        p1.setId((long) UUID.randomUUID().hashCode());
-        p1.setName("Tompkins");
-        p1.setAge(22);
-        int i = personService.addPerson(p1);
-        System.out.println("addPerson>>>" + i);
+    public Integer addPerson(@RequestBody Person person) {
+        return personService.addPerson(person);
+    }
+
+    @PostMapping("/delete")
+    public Integer deletePerson(@RequestBody Person person) {
+        return personService.deletePersonById(person.getId());
     }
 }
--- spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/service/PersonService.java
+++ spring-boot/spring-boot-jdbc/src/main/java/com/datawisher/spring/boot/jdbc/service/PersonService.java
@@ -5,6 +5,7 @@ import com.datawisher.spring.boot.jdbc.model.Person;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PersonService {
