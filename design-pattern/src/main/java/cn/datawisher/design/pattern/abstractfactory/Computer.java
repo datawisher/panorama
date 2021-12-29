7044d68822f6885197fef1b79484c8e12c3a3ac4
Jim Han <Jim.Han2@Honeywell.com>
2021-12-29 22:09:06 +0800

    [chore] update

---
+++ design-pattern/src/main/java/cn/datawisher/design/pattern/abstractfactory/Computer.java
@@ -0,0 +1,10 @@
+package cn.datawisher.design.pattern.abstractfactory;
+
+/**
+ * @author h407644
+ * @date 2021-12-29
+ */
+public interface Computer {
+
+    String internet();
+}
---
+++ design-pattern/src/main/java/cn/datawisher/design/pattern/abstractfactory/XiaomiComputer.java
@@ -0,0 +1,13 @@
+package cn.datawisher.design.pattern.abstractfactory;
+
+/**
+ * @author h407644
+ * @date 2021-12-29
+ */
+public class XiaomiComputer implements Computer{
+
+    @Override
+    public String internet() {
+        return "surf the internet by xiaomi computer.";
+    }
+}
