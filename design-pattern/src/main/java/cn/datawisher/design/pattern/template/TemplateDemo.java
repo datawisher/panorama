2587eaf90f90a89438529d2f535d568db1496a92
Jim Han <Jim.Han2@Honeywell.com>
2022-01-29 13:57:32 +0800

    [feat] template

---
+++ design-pattern/src/main/java/cn/datawisher/design/pattern/template/TemplateDemo.java
@@ -0,0 +1,16 @@
+package cn.datawisher.design.pattern.template;
+
+/**
+ * @author h407644
+ * @date 2022-01-29
+ */
+public class TemplateDemo {
+
+    public static void main(String[] args) {
+        AbstractTemplate template1 = new TakeMoney();
+        template1.templateMethod();
+
+        AbstractTemplate template2 = new SaveMoney();
+        template2.templateMethod();
+    }
+}
