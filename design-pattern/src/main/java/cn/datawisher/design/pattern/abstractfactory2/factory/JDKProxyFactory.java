b8ef3a09c05e1805be29eadee2bf30dc5e74d7d1
Jim Han <jim.han2@honeywell.com>
2022-02-13 22:54:34 +0800

    [feat] abstract factory with jdk proxy

---
+++ design-pattern/src/main/java/cn/datawisher/design/pattern/abstractfactory2/factory/JDKProxyFactory.java
@@ -0,0 +1,19 @@
+package cn.datawisher.design.pattern.abstractfactory2.factory;
+
+import cn.datawisher.design.pattern.abstractfactory2.workshop.ICacheAdapter;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author h407644
+ * @date 2022-02-09
+ */
+public class JDKProxyFactory {
+
+    public static <T> T getProxy(Class cacheClazz, Class<? extends ICacheAdapter> cacheAdapter) throws Exception {
+        InvocationHandler handler = new JDKInvocationHandler(cacheAdapter.newInstance());
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        return (T) Proxy.newProxyInstance(classLoader, new Class[]{cacheClazz}, handler);
+    }
+}
---
+++ design-pattern/src/main/java/cn/datawisher/design/pattern/abstractfactory2/workshop/impl/EGMCacheAdapter.java
@@ -0,0 +1,35 @@
+package cn.datawisher.design.pattern.abstractfactory2.workshop.impl;
+
+import cn.datawisher.design.pattern.abstractfactory2.rediscluster.EGM;
+import cn.datawisher.design.pattern.abstractfactory2.workshop.ICacheAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author h407644
+ * @date 2022-02-09
+ */
+public class EGMCacheAdapter implements ICacheAdapter {
+
+    private final EGM egm = new EGM();
+
+    @Override
+    public String get(String key) {
+        return egm.gain(key);
+    }
+
+    @Override
+    public void set(String key, String value) {
+        egm.set(key, value);
+    }
+
+    @Override
+    public void set(String key, String value, long timeout, TimeUnit timeUnit) {
+        egm.setEx(key, value, timeout, timeUnit);
+    }
+
+    @Override
+    public void del(String key) {
+        egm.delete(key);
+    }
+}
