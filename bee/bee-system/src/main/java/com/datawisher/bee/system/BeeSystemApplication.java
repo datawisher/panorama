d5d4cff1431a386fd62ad972851242a8562fcda2
Jim Han <datawisher@gmail.com>
2022-09-10 00:47:07 +0800

    feat: add system module

---
+++ bee/bee-system/src/main/java/com/datawisher/bee/system/BeeSystemApplication.java
@@ -0,0 +1,16 @@
+package com.datawisher.bee.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author h407644
+ * @date 2022-09-10
+ */
+@SpringBootApplication
+public class BeeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeSystemApplication.class, args);
+    }
+}
