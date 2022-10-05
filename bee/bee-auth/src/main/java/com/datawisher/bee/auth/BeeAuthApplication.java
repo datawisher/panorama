38943f1db3cd5274a826f89b537f93d5f00b828f
Jim Han <datawisher@gmail.com>
2022-10-05 09:11:18 +0800

    feat: add auth server

---
+++ bee/bee-auth/src/main/java/com/datawisher/bee/auth/BeeAuthApplication.java
@@ -0,0 +1,16 @@
+package com.datawisher.bee.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author h407644
+ * @date 2022-10-04
+ */
+@SpringBootApplication
+public class BeeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeAuthApplication.class, args);
+    }
+}
