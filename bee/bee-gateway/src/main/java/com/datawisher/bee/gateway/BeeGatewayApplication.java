0a89ef3eedcf729d66ec9d9b0c6a4c0a346ea99f
Jim Han <datawisher@gmail.com>
2022-12-08 22:03:34 +0800

    feat: gateway

---
+++ bee/bee-gateway/src/main/java/com/datawisher/bee/gateway/BeeGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.datawisher.bee.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author h407644
+ * @date 2022-12-08
+ */
+@SpringBootApplication
+public class BeeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeGatewayApplication.class, args);
+    }
+}
