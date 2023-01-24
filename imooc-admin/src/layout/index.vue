c326fbbb6515f791400615ff1c7d8dc7fc0f428c
Jim Han <datawisher@gmail.com>
2023-01-24 12:16:43 +0800

    feat: layout scss

--- imooc-admin/src/layout/index.vue
+++ imooc-admin/src/layout/index.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="">Layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+     <sidebar class="sidebar-container"></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部 navbar-->
+        <navbar />
+      </div>
+      <!-- 内容区 -->
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import Navbar from '@/layout/components/Navbar.vue'
+import Sidebar from '@/layout/components/Sidebar/index.vue'
+import AppMain from '@/layout/components/AppMain.vue'
+
 </script>
 
 <style lang="scss" scoped>
+@import "~@/styles/mixin.scss";
+@import "~@/styles/variables.scss";
 
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
 </style>
