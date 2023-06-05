f5b28f76634e15a50a62b82783d5ff03b830d6ec
Jim Han <datawisher@gmail.com>
2023-06-05 22:37:21 +0800

    test: svg test

--- vue3_admin_template/project/src/App.vue
+++ vue3_admin_template/project/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <h3>我是APP</h3>
+    <h3> SVG 图标测试 </h3>
+    <svg>
+      <use xlink:href="#icon-dropbox" fill="red"></use>
+    </svg>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue'
 
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vue3_admin_template/project/src/main.ts
+++ vue3_admin_template/project/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
+// 引入 ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入全局的 SVG 图标
+import 'virtual:svg-icons-register'
 
-createApp(App).mount('#app')
+
+console.log(import.meta.env)
+
+const app = createApp(App)
+// app.use(ElementPlus, {
+//     locale: zhCn 
+// })
+app.mount('#app')
