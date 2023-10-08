5a8f66d4962948c30ccf081875c56accbea00cfc
Jim Han <datawisher@gmail.com>
2023-10-08 21:26:54 +0800

    test: js style 7 - wrap-iife & no-loop-func

--- javascript_ex2/style07.js
+++ javascript_ex2/style07.js
@@ -21,3 +21,43 @@ const short = function longUniqueMoreDescriptiveLexicalFoo() {
 };
 
 
+// 7.2 Wrap立即调用函数表达式。 eslint: wrap-iife
+// 为什么? 立即调用的函数表达式是单个单元 - 包装， 并且拥有括号调用, 在括号内, 清晰的表达式。
+// 请注意，在一个到处都是模块的世界中，您几乎不需要一个 IIFE 。
+// immediately-invoked function expression (IIFE) 立即调用的函数表达式
+(function () {
+    console.log('Welcome to the Internet. Please follow me.');
+}());
+
+
+// 7.3 切记不要在非功能块中声明函数 (if, while, 等)。 将函数赋值给变量。
+// 浏览器允许你这样做，但是他们都有不同的解释，这是个坏消息。 eslint: no-loop-func
+
+
+// 7.4 注意: ECMA-262 将 block 定义为语句列表。 函数声明不是语句。
+// bad
+if (currentUser) {
+    function test() {
+        console.log('Nope.');
+    }
+}
+
+// good
+let test;
+if (currentUser) {
+    test = () => {
+        console.log('Yup.');
+    };
+}
+
+
+// 7.5 永远不要定义一个参数为 arguments。 这将会优先于每个函数给定范围的 arguments 对象。
+// bad
+function foo(name, options, arguments) {
+    // ...
+}
+
+// good
+function foo(name, options, args) {
+    // ...
+}
