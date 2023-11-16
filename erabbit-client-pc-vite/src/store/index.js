c3c0123f9c549712dbaaf9b1c9adb1d30100c0a0
Jim Han <datawisher@gmail.com>
2023-11-16 22:48:43 +0800

    not use ts

---
+++ erabbit-client-pc-vite/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  // state() {
+  //     return {
+  //         username: 'tom'
+  //     }
+  // },
+  state: { username: 'tom' },
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
+
+export default store
