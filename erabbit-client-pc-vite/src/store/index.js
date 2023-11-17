d36e429840aa293ad6a9718721324b07c2f4443e
Jim Han <datawisher@gmail.com>
2023-11-17 22:50:36 +0800

    feat: vuex-persistedState plugins configuration

--- erabbit-client-pc-vite/src/store/index.js
+++ erabbit-client-pc-vite/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import cart from '@/store/modules/cart'
 import user from '@/store/modules/user'
 import category from '@/store/modules/category'
@@ -8,7 +9,17 @@ const store = createStore({
     cart,
     user,
     category
-  }
+  },
+  // 配置插件
+  plugins: [
+    // 默认存储在local storage上
+    createPersistedState({
+      // 本地存储的名字
+      key: 'erabbit-client-pc-vite-store',
+      // 指定需要存储的模块
+      paths: ['user', 'cart']
+    })
+  ]
 })
 
 // state() {
