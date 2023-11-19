2059cedd49619baf1a09f3f4c60a17a2d348e711
Jim Han <datawisher@gmail.com>
2023-11-19 11:43:54 +0800

    fix: state use computed as reactive data

--- erabbit-client-pc-vite/src/components/app-topnav.vue
+++ erabbit-client-pc-vite/src/components/app-topnav.vue
@@ -2,24 +2,29 @@
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <li>
-          <a href="javascript:"><i class="iconfont icon-user"></i>{{ profile.account }}</a>
-        </li>
-        <li><a href="javascript:">退出登录</a></li>
-        <li><a href="javascript:">请先登录</a></li>
-        <li><a href="javascript:">免费注册</a></li>
+        <template v-if="profile.token">
+          <li>
+            <a href="javascript:"><i class="iconfont icon-user"></i>{{ profile.account }}</a>
+          </li>
+          <li><a href="javascript:">退出登录</a></li>
+        </template>
+        <template v-else>
+          <li><a href="javascript:">请先登录</a></li>
+          <li><a href="javascript:">免费注册</a></li>
+        </template>
         <li><a href="javascript:">我的订单</a></li>
         <li><a href="javascript:">会员中心</a></li>
         <li><a href="javascript:">帮助中心</a></li>
         <li><a href="javascript:">关于我们</a></li>
         <li>
-          <a href="javascript:;"><i class="iconfont icon-phone"></i>手机版</a>
+          <a href="javascript:"><i class="iconfont icon-phone"></i>手机版</a>
         </li>
       </ul>
     </div>
   </nav>
 </template>
 <script>
+import { computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
@@ -27,7 +32,9 @@ export default {
   setup() {
     // 根据当前的登录状态显示 用户名和退出登录
     const store = useStore()
-    const { profile } = store.state.user
+    const profile = computed(() => {
+      return store.state.user.profile
+    })
     return { profile }
   }
 }
