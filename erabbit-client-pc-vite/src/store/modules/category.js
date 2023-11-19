2a27221c00204350bf4132b8887e52b51eb91cea
Jim Han <datawisher@gmail.com>
2023-11-19 21:45:51 +0800

    feat: rendering of header category navigation

--- erabbit-client-pc-vite/src/store/modules/category.js
+++ erabbit-client-pc-vite/src/store/modules/category.js
@@ -1,9 +1,26 @@
 // 商品分类模块
+import { topCategory } from '@/api/constants'
+import { findAllCategory } from '@/api/category'
+
 export default {
   namespaced: true,
   state() {
     return {
-      list: []
+      // 分类信息集合，依赖topCategory重新设置，保证初始化就有数据，不至于白屏
+      list: topCategory.map((item) => ({ name: item }))
+    }
+  },
+  // 修改分类函数
+  mutations: {
+    setList(state, payload) {
+      state.list = payload
+    }
+  },
+  // 获取分类函数
+  actions: {
+    async getList({ commit }) {
+      const data = await findAllCategory()
+      commit('setList', data.result)
     }
   }
 }
