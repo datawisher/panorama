2d33ce36c53b686ad6d11fd463d39d4f30bc32bf
Jim Han <datawisher@gmail.com>
2023-11-21 21:57:54 +0800

    feat: header sticky, add @vueuse/core

--- erabbit-client-pc-vite/src/views/Layout.vue
+++ erabbit-client-pc-vite/src/views/Layout.vue
@@ -1,6 +1,7 @@
 <template>
   <AppTopnav />
   <AppHeader />
+  <AppHeaderSticky />
   <!--  内容容器-->
   <div class="main">
     <!--    二级路由-->
@@ -12,11 +13,12 @@
 import AppTopnav from '@/components/app-topnav.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '@/components/app-footer.vue'
+import AppHeaderSticky from '@/components/app-header-sticky.vue'
 import { useStore } from 'vuex'
 
 export default {
   name: 'Layout',
-  components: { AppTopnav, AppHeader, AppFooter },
+  components: { AppHeaderSticky, AppTopnav, AppHeader, AppFooter },
   // 获取分类数据
   setup() {
     const store = useStore()
