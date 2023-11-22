e431707d68d536d770b7a73a67b2bcfb850e6502
Jim Han <datawisher@gmail.com>
2023-11-22 22:56:29 +0800

    feat: add home-category

--- erabbit-client-pc-vite/src/views/home/index.vue
+++ erabbit-client-pc-vite/src/views/home/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container">
-    HomePage
+    <!--    首页入口-->
+    <div class="home-entry">
+      <!--      左侧分类-->
+      <HomeCategory />
+      <!--      轮播图-->
+    </div>
   </div>
 </template>
 
 <script>
+import HomeCategory from '@/views/home/components/home-category.vue'
+
 export default {
-  name:'HomePage'
+  name: 'HomePage',
+  components: { HomeCategory }
 }
 </script>
 
