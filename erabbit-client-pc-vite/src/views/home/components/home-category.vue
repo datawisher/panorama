d551ca5444c2ad4f02d95beec0095a6a07c1d79b
Jim Han <datawisher@gmail.com>
2023-11-23 22:38:57 +0800

    refactor: home-category data render

--- erabbit-client-pc-vite/src/views/home/components/home-category.vue
+++ erabbit-client-pc-vite/src/views/home/components/home-category.vue
@@ -1,19 +1,81 @@
 <template>
   <div class="home-category">
     <ul class="menu">
-      <li v-for="i in 10" :key="i">
-        <RouterLink to="/">居家</RouterLink>
-        <RouterLink to="/">洗漱</RouterLink>
-        <RouterLink to="/">清洁</RouterLink>
+      <li v-for="item in menuList" :key="item.id">
+        <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+        <template v-if="item.children">
+          <RouterLink v-for="sub in item.children" :key="sub.id" :to="`/category/sub/${sub.id}`"
+            >{{ sub.name }}
+          </RouterLink>
+        </template>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { computed, reactive } from 'vue'
+import { useStore } from 'vuex'
+
 export default {
-  name: 'HomeCategory'
+  name: 'HomeCategory',
+  setup() {
+    const store = useStore()
+    const brand = reactive({
+      id: 'brand',
+      name: '品牌',
+      children: [
+        {
+          id: 'brand-children',
+          name: '品牌推荐'
+        }
+      ]
+    })
+    const menuList = computed(() => {
+      // 得到9个分类及其下的2个子分类
+      const list = store.state.category.list.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          children: item.children && item.children.slice(0, 2),
+          goods: item.goods
+        }
+      })
+      list.push(brand)
+      return list
+    })
+    return { menuList }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-category {
+  width: 250px;
+  height: 500px;
+  background: rgba(0, 0, 0, 0.8);
+  position: relative;
+  z-index: 99;
+
+  .menu {
+    li {
+      padding-left: 40px;
+      height: 50px;
+      line-height: 50px;
+
+      &:hover {
+        background: @xtxColor;
+      }
+
+      a {
+        margin-right: 4px;
+        color: #fff;
+
+        &:first-child {
+          font-size: 16px;
+        }
+      }
+    }
+  }
+}
+</style>
