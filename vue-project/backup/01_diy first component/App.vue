9aaa5315bfe22446f207c49fe602fe0a18582a65
Jim Han <datawisher@gmail.com>
2023-12-26 23:06:16 +0800

    vue2024ex001

---
+++ vue-project/backup/01_diy first component/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>hola!!!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name:'App'
+}
+</script>
+
+<style scoped>
+.app{
+  background-color: pink;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
