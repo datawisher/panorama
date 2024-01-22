583d819060982af2812e55a5c787fd4cf41dd833
Jim Han <datawisher@gmail.com>
2024-01-22 21:33:36 +0800

    javascript base - instanceof and Constructor inheritance

--- javascript-base/app603.js
+++ javascript-base/app603.js
@@ -149,3 +149,72 @@ function Foo() {
 
 var f = new Foo()
 f.constructor.name // "Foo"
+
+
+// 2.instanceof 运算符
+// instanceof运算符返回一个布尔值，表示对象是否为某个构造函数的实例。
+
+// 对象v是构造函数Vehicle的实例，所以返回true
+var v = new Vehicle();
+v instanceof Vehicle // true
+
+// Vehicle是对象v的构造函数，它的原型对象是Vehicle.prototype，
+// isPrototypeOf()方法是 JavaScript 提供的原生方法，用于检查某个对象是否为另一个对象的原型
+v instanceof Vehicle
+// 等同于
+Vehicle.prototype.isPrototypeOf(v)
+
+// d同时是Date和Object的实例，因此对这两个构造函数都返回true
+var d = new Date();
+d instanceof Date // true
+d instanceof Object // true
+
+// 除了null，其他对象的instanceOf Object的运算结果都是true。
+var obj = {foo: 123};
+obj instanceof Object // true
+null instanceof Object // false
+
+// Object.create(null)返回一个新对象obj，它的原型是null（Object.create()的详细介绍见后文）。
+// 右边的构造函数Object的prototype属性，不在左边的原型链上，因此instanceof就认为obj不是Object的实例。
+// 这是唯一的instanceof运算符判断会失真的情况（一个对象的原型是null）
+var obj = Object.create(null);
+typeof obj // "object"
+obj instanceof Object // false
+
+// instanceof运算符判断，变量x是数组，变量y是对象
+var x = [1, 2, 3];
+var y = {};
+x instanceof Array // true
+y instanceof Object // true
+
+// instanceof运算符只能用于对象，不适用原始类型的值
+var s = 'hello';
+s instanceof String // false
+
+// 对于undefined和null，instanceof运算符总是返回false
+undefined instanceof Object // false
+null instanceof Object // false
+
+// 在函数体内部判断this关键字是否为构造函数Fubar的实例。如果不是，就表明忘了加new命令
+function Fubar(foo, bar) {
+    if (this instanceof Fubar) {
+        this._foo = foo;
+        this._bar = bar;
+    } else {
+        return new Fubar(foo, bar);
+    }
+}
+
+
+// 3. 构造函数的继承
+// Sub是子类的构造函数，this是子类的实例。在实例上调用父类的构造函数Super，就会让子类实例具有父类实例的属性
+function Sub(value) {
+    Super.call(this);
+    this.prop = value;
+}
+
+// Sub.prototype是子类的原型，要将它赋值为Object.create(Super.prototype)，
+// 而不是直接等于Super.prototype。否则后面两行对Sub.prototype的操作，会连父类的原型Super.prototype一起修改掉
+Sub.prototype = Object.create(Super.prototype);
+Sub.prototype.constructor = Sub;
+Sub.prototype.method = '...';
