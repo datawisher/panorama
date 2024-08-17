4d250858411ac6629e912ab2fb7ab4839de10c72
Jim Han <datawisher@gmail.com>
2024-08-17 21:09:40 +0800

    feat: toRefs and toRef

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <!--  <Person/>-->
   <!--  <Car/>-->
-  <Car2/>
+  <!-- <Car2/> -->
+  <Person2 />
 </template>
 
 <script>
 import Person from "@/components/Person.vue";
+import Person2 from "@/components/Person2.vue";
 import Car from "@/components/Car.vue";
 import Car2 from "@/components/Car2.vue";
 
 export default {
   name: "App",
-  components: {Car2, Car, Person}
+  components: { Car2, Car, Person, Person2 }
 }
 </script>
 
---
+++ hello_vue3/src/components/Person2.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="person2">
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+    </div>
+</template>
+
+<script setup lang="ts" name="Person2"">
+import { reactive, toRefs, toRef } from "vue"; 
+
+let person = reactive({ name: "John", age: 30 });
+
+let {name, age} = toRefs(person);
+
+let age2 = toRef(person, 'age');
+console.log(age2.value)
+
+const changeName = () => {
+    name.value += '!';
+};
+
+const changeAge = () => {
+    age.value += 1
+};
+</script>
+
+<style scoped>
+.person2 {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
