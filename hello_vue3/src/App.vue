0d66af58324366c99b577d310ce33832cf77822e
Jim Han <datawisher@gmail.com>
2024-08-18 10:44:19 +0800

    feat: watch situation 3

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <Person5 />
+  <Person6 />
 </template>
 
 <script>
-import Person from "@/components/Person.vue";
-import Person2 from "@/components/Person2.vue";
-import Person3 from "@/components/Person3.vue";
-import Person4 from "@/components/Person4.vue";
-import Person5 from "@/components/Person5.vue";
-import Car from "@/components/Car.vue";
-import Car2 from "@/components/Car2.vue";
+  import Person6 from "@/components/Person6.vue";
 
-export default {
-  name: "App",
-  components: { Car2, Car, Person, Person2, Person3, Person4, Person5 }
-}
+  export default {
+    name: "App",
+    components: { Person6 }
+  }
 </script>
 
 <style>
-.app {
-  background-color: #ddd;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
 </style>
---
+++ hello_vue3/src/components/Person6.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="person6">
+        <h1>情况三：监视【reactive】定义的【对象类型】数据</h1>
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click="changeName">修改姓名</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changePerson">修改整个人</button>
+    </div>
+</template>
+
+<script setup lang="ts" name="Person6">
+    import { reactive, watch } from "vue";
+
+    // data
+    let person = reactive({
+        name: "张三",
+        age: 18
+    });
+
+    // function
+    function changeName() {
+        person.name += "!";
+    }
+    function changeAge() {
+        person.age += 1;
+    }
+    function changePerson() {
+        // person = {
+        //     name: "李四",
+        //     age: 20
+        // };
+        Object.assign(person, {
+            name: "王五",
+            age: 30
+        });
+    }
+
+    // watch
+    // 情况三：监视【reactive】定义的【对象类型】数据，且默认是开启深度监视的
+    watch(person, (newValue, oldValue) => {
+        console.log("监视【ref】定义的【对象类型】数据", newValue, oldValue);
+    });
+
+
+</script>
+
+<style scoped>
+    .person6 {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
