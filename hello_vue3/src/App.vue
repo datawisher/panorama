fda129c81f2db9bd0027994425c532751d16de33
Jim Han <datawisher@gmail.com>
2024-08-18 09:37:11 +0800

    feat: watch situation 1

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,21 +1,18 @@
 <template>
-  <!--  <Person/>-->
-  <!--  <Car/>-->
-  <!-- <Car2/> -->
-  <!-- <Person2 /> -->
-  <Person3 />
+  <Person4 />
 </template>
 
 <script>
 import Person from "@/components/Person.vue";
 import Person2 from "@/components/Person2.vue";
 import Person3 from "@/components/Person3.vue";
+import Person4 from "@/components/Person4.vue";
 import Car from "@/components/Car.vue";
 import Car2 from "@/components/Car2.vue";
 
 export default {
   name: "App",
-  components: { Car2, Car, Person, Person2, Person3 }
+  components: { Car2, Car, Person, Person2, Person3, Person4 }
 }
 </script>
 
---
+++ hello_vue3/src/components/Person4.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="person4">
+        <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+        <h2>当前求和为：{{ sum }}</h2>
+        <button @click="changeSum">+1</button>
+    </div>
+</template>
+
+<script setup lang="ts" name="Person4">
+    import { ref, watch } from "vue";
+
+    // data
+   let sum = ref(0);
+
+   // function
+   function changeSum() {
+       sum.value += 1;
+   }
+
+   // watch
+   // 情况一：监视【ref】定义的【基本类型】数据
+   const stopWatch = watch(sum, (newValue, oldValue) => {
+       console.log(newValue, oldValue);
+       if (newValue >= 10) {
+           stopWatch();
+       }
+   })
+   
+</script>
+
+<style scoped>
+    .person4 {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
