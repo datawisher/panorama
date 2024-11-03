083bfb99c1d149d73504d5c860fdd5a72e6fdbcb
Jim Han <datawisher@gmail.com>
2024-11-03 19:41:20 +0800

    Guava List: CharacterList from String

--- guava_modules/src/test/java/com/datawisher/panorama/guava/GuavaListTest.java
+++ guava_modules/src/test/java/com/datawisher/panorama/guava/GuavaListTest.java
@@ -4,22 +4,14 @@ package com.datawisher.panorama.guava;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.collect.Lists;
 
 public class GuavaListTest {
-
-    @BeforeAll
-    public static void beforeAll() {
-        List<String> names = Lists.newArrayList("John", "Adam", "Jane");
-    }
-
 
     @Test
     public void whenReverseList_thenReversed() {
@@ -27,4 +19,13 @@ public class GuavaListTest {
         List<String> reversed = Lists.reverse(names);
         assertThat(reversed, contains("Jane", "Adam", "John"));
     }
+
+    @Test
+    public void whenCreateCharacterListFromString_thenCreated() {
+        List<Character> chars = Lists.charactersOf("John");
+
+        assertEquals(4, chars.size());
+        assertThat(chars, contains('J', 'o', 'h', 'n'));
+    }
+
 }
